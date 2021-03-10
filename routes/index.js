f6d9f48d5f529a4cb4245c2102b7d28116a0d3e7
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-03-10 12:23:22 -0300

    agora a validação tem layout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const Cliente = require("../models/cliente")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post('/validar', function(req, res, next) {
+  let nome = req.body.nome
+  let cpf = req.body.cpf
+  let cliente = new Cliente(nome, cpf)
+  res.render('validado', { nome: nome, cpf: cpf, validado: cliente.validarCPF() });
 });
 
 module.exports = router;
